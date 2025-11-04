5589cab69caaa6c8ff6561bd3a85d1d914accc63
quanlee214 <22120290@student.hcmus.edu.vn>
2025-11-05 01:25:40 +0700

    fix deploy do push nham git

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import Calculator from './components/calculator'
 
-
 function App() {
   return (
     <main className="min-h-screen bg-gradient-to-br from-slate-900 to-slate-800 flex items-center justify-center p-4">
